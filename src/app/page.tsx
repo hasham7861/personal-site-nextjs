72de053990029b0511ddd06fb411310b36be58a1
Hasham <hashamsp36@gmail.com>
2025-12-09 18:33:36 -0500

    header fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
-    <div className="ml-64 p-8 mt-5 bg-background text-foreground min-h-screen">
-      <p className="text-[20px] font-medium mb-2">Long time, no C 👋</p>
-      <h1 className="text-5xl font-bold mb-1">I'm Hasham Alam</h1>
-      <p className="text-[30px] font-semibold mb-2">Software Engineer</p>
-      <p className="text-[18px] font-medium">Knock Knock. Race Condition. Who's there?</p>
+    <div className="ml-64 p-10 bg-background text-foreground min-h-screen">
+      <div className="pt-2">
+        <p className="text-[20px] font-medium mb-2">Long time, no C 👋</p>
+        <h1 className="text-5xl font-bold mb-1">I'm Hasham Alam</h1>
+        <p className="text-[30px] font-semibold mb-2">Software Engineer</p>
+        <p className="text-[18px] font-medium">Knock Knock. Race Condition. Who's there?</p>
+      </div>
     </div>
   );
 }
